93f6ed580a8e189eb1b5f1e4d90808dacf37bb0a
Md. Atiqur Rahman Sifat <mdatiqur.2510@gmail.com>
2026-04-26 20:39:27 +0600

    Read CSV file along with importing libraries.

---
+++ modelTrain_VS.py
@@ -0,0 +1,8 @@
+# SeismoGuard
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Showing the first few rows of the dataset to understand its structure and contents.
+df = pd.read_csv('D:/Capstone/STEAD Dataset/merge.csv/merge.csv')
+print(df.head())

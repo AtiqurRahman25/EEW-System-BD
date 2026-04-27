abd0544e0d7c6337a0dc6b549eb455a25c543be1
Md. Atiqur Rahman Sifat <mdatiqur.2510@gmail.com>
2026-04-27 14:23:20 +0600

    Extracting informations of each column.

--- modelTrain_VS.py
+++ modelTrain_VS.py
@@ -5,4 +5,9 @@ import matplotlib.pyplot as plt
 
 # Showing the first few rows of the dataset to understand its structure and contents.
 df = pd.read_csv('D:/Capstone/STEAD Dataset/merge.csv/merge.csv')
-print(df.head())
+# print(df.head())
+
+# df.info()
+
+eq_df = df[df['trace_category'] == 'earthquake_local'].sample(5000, random_state=42)
+noise_df = df[df['trace_category'] == 'noise'].sample(5000, random_state=42)

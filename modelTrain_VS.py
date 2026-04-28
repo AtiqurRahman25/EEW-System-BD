312e8a7c13cb1373740cdb321fedc427dc746414
Atiqur Rahman Sifat <107290003+AtiqurRahman25@users.noreply.github.com>
2026-04-28 19:59:03 +0600

    Data preprocessing for earthquake and noise
    Comment out earthquake and noise data preprocessing code.

--- modelTrain_VS.py
+++ modelTrain_VS.py
@@ -9,5 +9,14 @@ df = pd.read_csv('D:/Capstone/STEAD Dataset/merge.csv/merge.csv')
 
 # df.info()
 
-eq_df = df[df['trace_category'] == 'earthquake_local'].sample(5000, random_state=42)
-noise_df = df[df['trace_category'] == 'noise'].sample(5000, random_state=42)
+# eq_df = df[df['trace_category'] == 'earthquake_local'].sample(5000, random_state=42)
+# noise_df = df[df['trace_category'] == 'noise'].sample(5000, random_state=42)
+
+# eq_df
+# noise_df
+
+# print("Missing values in earthquake_df:")
+# print(eq_df.isnull().sum())
+
+# print("\nMissing values in noise_df:")
+# print(noise_df.isnull().sum())
